2c46c23e62bfea1a44523db85b03b3622ffe52c3
gopivarmaa <gopivarmaa@gmail.com>
2020-01-12 21:04:20 +0530

    test3

--- GitPractice/src/Test1.java
+++ GitPractice/src/Test1.java
@@ -3,9 +3,9 @@ public class Test1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("test1 merge");
-		
 		System.out.println("test1");
+		
+		System.out.println("test12");
 	}
 	
 
